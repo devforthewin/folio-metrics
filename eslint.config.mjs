107e428732a865cd95d969e4fd8ef0a95b68d9c7
arbuz.tp <arbuz.tp@gmail.com>
2025-07-07 21:00:17 +0300

    chore(lint): configurate ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,43 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+        },
+      ],
+      'max-len': ['warn', { code: 120 }],
+      'comma-dangle': ['warn', 'always-multiline'],
+      // 'no-unused-vars': 'warn',
+      // 'no-console': 'warn',
+      semi: ['warn', 'never'],
+      quotes: ['warn', 'single'],
+      indent: ['warn', 2],
+      'no-trailing-spaces': ['error', { skipBlankLines: false }],
+      'no-multi-spaces': ['error', { ignoreEOLComments: false }],
+      'object-curly-spacing': ['error', 'always'],
+
+      'jsx-quotes': ['warn', 'prefer-double'],
+      'react/jsx-indent': ['warn', 2],
+      'react/jsx-indent-props': ['warn', 2],
+      'react/jsx-first-prop-new-line': ['warn', 'multiline'],
+      'react/jsx-max-props-per-line': ['warn', { maximum: 2 }],
+    },
+  },
 ];
 
 export default eslintConfig;
