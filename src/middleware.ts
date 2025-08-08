2388e33f4749958efdd3a8e9a748f20c412e41d3
arbuz.tp <arbuz.tp@gmail.com>
2025-08-08 12:59:11 +0300

    feat(middleware): add admin route middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,23 @@ const PUBLIC_FILE = /\.(.*)$/
 const DEFAULT_LOCALE = 'en'
 
 export function middleware(request: NextRequest) {
+  const authToken = request.cookies.get('admin-auth')?.value
+
   const { pathname } = request.nextUrl
 
-  // Оставляем API, админку и файлы без изменений
+  //Check if the current page is a protected admin page
+  const isAdminPage = pathname.startsWith('/admin') && pathname !== '/admin/login'
+
+  //if there is no token and the page is not protected
+  if (isAdminPage && !authToken) {
+    // create an absolute URL for redirect
+    const loginParh = new URL('/admin/login', request.url)
+    return NextResponse.redirect(loginParh)
+  }
+
+  // Skipping anything that looks like Next.js files or service paths
   if (
     pathname.startsWith('/api') ||
-    pathname.startsWith('/admin') ||
     pathname.startsWith('/_next') ||
     pathname.startsWith('/favicon.ico') ||
     PUBLIC_FILE.test(pathname)
@@ -17,6 +28,7 @@ export function middleware(request: NextRequest) {
     return NextResponse.next()
   }
 
+  //Check if locale is missing from path (for all public pages)
   const pathnameIsMissingLocale = /^\/(?!en|ru)(.*)/.test(pathname)
   if (pathnameIsMissingLocale) {
     const url = request.nextUrl.clone()
@@ -27,8 +39,7 @@ export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
 
+//Run middleware on all paths except static files and api
 export const config = {
-  matcher: ['/', '/((?!api|_next|favicon.ico|admin).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
-
-// matcher: ['/((?!api|_next/static|_next/image|admin|favicon.ico).*)'],
