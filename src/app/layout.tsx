72a9523958df33c5a662b1da5f7d341e83acda58
arbuz.tp <arbuz.tp@gmail.com>
2025-09-29 16:05:44 +0300

    feat: add root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Inter } from 'next/font/google'
+
+import type { Metadata } from 'next'
+import './../styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Folio-Metrics',
+  description: 'Track your portfolio with ease',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
