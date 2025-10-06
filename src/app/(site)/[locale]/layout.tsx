92b6c29bb8224bdfda3fd4205d47a0d150e94d47
arbuz.tp <arbuz.tp@gmail.com>
2025-10-06 12:32:56 +0300

    chore: clean up code, fix typos, and update metadata

--- src/app/(site)/[locale]/layout.tsx
+++ src/app/(site)/[locale]/layout.tsx
@@ -1,14 +1,14 @@
-import '../../../styles/globals.css'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
 
 import { robotoMono, robotoSlab } from '@/lib/fonts'
+import '../../../lib/fontawesome'
 
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Folio-Metrics',
-  description: 'Track your portfolio with ease',
+  title: 'Arbuz Tanya | Full-Stack Developer',
+  description: 'Hi! This is my live resume and a demonstration of my development skills. The project is built on Next.js and includes custom analytics.',
 }
 
 export default async function RootLayout({
@@ -23,9 +23,11 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
-      <NextIntlClientProvider locale={locale} messages={messages}>
-        <body className={`${robotoSlab.variable} ${robotoMono.variable} antialiased`}>{children}</body>
-      </NextIntlClientProvider>
+      <body className={`${robotoSlab.variable} ${robotoMono.variable} antialiased`}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
     </html>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import './../styles/globals.css'
-import { robotoMono, robotoSlab } from '@/lib/fonts'
-
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Folio-Metrics',
-  description: 'Track your portfolio with ease',
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={`${robotoSlab.variable} ${robotoMono.variable} antialiased`}>{children}</body>
-    </html>
-  )
+  return children
 }
